74584426875499453e84fa39e497cc0ff13138c2
ichiyou-tori <kanon.etori@gmail.com>
2024-11-21 17:05:55 +0900

    メモ帳アプリのアップロード

---
+++ memo/memo/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace memo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.新規作成ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.新しいウィンドウToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開くToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.名前を付けて保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ウィンドウを閉じるToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.終了ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.表示ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.太字ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.ズームToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.拡大ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.縮小ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.標準ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルToolStripMenuItem,
+            this.表示ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            this.ファイルToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.新規作成ToolStripMenuItem,
+            this.新しいウィンドウToolStripMenuItem,
+            this.開くToolStripMenuItem,
+            this.保存ToolStripMenuItem1,
+            this.名前を付けて保存ToolStripMenuItem,
+            this.ウィンドウを閉じるToolStripMenuItem,
+            this.終了ToolStripMenuItem1});
+            this.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            this.ファイルToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ファイルToolStripMenuItem.Text = "ファイル";
+            // 
+            // 新規作成ToolStripMenuItem
+            // 
+            this.新規作成ToolStripMenuItem.Name = "新規作成ToolStripMenuItem";
+            this.新規作成ToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.新規作成ToolStripMenuItem.Text = "新規作成";
+            this.新規作成ToolStripMenuItem.Click += new System.EventHandler(this.新規作成ToolStripMenuItem_Click);
+            // 
+            // 新しいウィンドウToolStripMenuItem
+            // 
+            this.新しいウィンドウToolStripMenuItem.Name = "新しいウィンドウToolStripMenuItem";
+            this.新しいウィンドウToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.新しいウィンドウToolStripMenuItem.Text = "新しいウィンドウ";
+            this.新しいウィンドウToolStripMenuItem.Click += new System.EventHandler(this.新しいウィンドウToolStripMenuItem_Click);
+            // 
+            // 開くToolStripMenuItem
+            // 
+            this.開くToolStripMenuItem.Name = "開くToolStripMenuItem";
+            this.開くToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.開くToolStripMenuItem.Text = "開く";
+            this.開くToolStripMenuItem.Click += new System.EventHandler(this.開くToolStripMenuItem_Click);
+            // 
+            // 保存ToolStripMenuItem1
+            // 
+            this.保存ToolStripMenuItem1.Name = "保存ToolStripMenuItem1";
+            this.保存ToolStripMenuItem1.Size = new System.Drawing.Size(161, 22);
+            this.保存ToolStripMenuItem1.Text = "保存";
+            this.保存ToolStripMenuItem1.Click += new System.EventHandler(this.保存ToolStripMenuItem1_Click);
+            // 
+            // 名前を付けて保存ToolStripMenuItem
+            // 
+            this.名前を付けて保存ToolStripMenuItem.Name = "名前を付けて保存ToolStripMenuItem";
+            this.名前を付けて保存ToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.名前を付けて保存ToolStripMenuItem.Text = "名前を付けて保存";
+            this.名前を付けて保存ToolStripMenuItem.Click += new System.EventHandler(this.名前を付けて保存ToolStripMenuItem_Click);
+            // 
+            // ウィンドウを閉じるToolStripMenuItem
+            // 
+            this.ウィンドウを閉じるToolStripMenuItem.Name = "ウィンドウを閉じるToolStripMenuItem";
+            this.ウィンドウを閉じるToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.ウィンドウを閉じるToolStripMenuItem.Text = "ウィンドウを閉じる";
+            this.ウィンドウを閉じるToolStripMenuItem.Click += new System.EventHandler(this.ウィンドウを閉じるToolStripMenuItem_Click);
+            // 
+            // 終了ToolStripMenuItem1
+            // 
+            this.終了ToolStripMenuItem1.Name = "終了ToolStripMenuItem1";
+            this.終了ToolStripMenuItem1.Size = new System.Drawing.Size(161, 22);
+            this.終了ToolStripMenuItem1.Text = "終了";
+            this.終了ToolStripMenuItem1.Click += new System.EventHandler(this.終了ToolStripMenuItem1_Click);
+            // 
+            // 表示ToolStripMenuItem
+            // 
+            this.表示ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ズームToolStripMenuItem,
+            this.太字ToolStripMenuItem});
+            this.表示ToolStripMenuItem.Name = "表示ToolStripMenuItem";
+            this.表示ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.表示ToolStripMenuItem.Text = "表示";
+            // 
+            // 太字ToolStripMenuItem
+            // 
+            this.太字ToolStripMenuItem.Name = "太字ToolStripMenuItem";
+            this.太字ToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.太字ToolStripMenuItem.Text = "太字";
+            this.太字ToolStripMenuItem.Click += new System.EventHandler(this.太字ToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 339);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(584, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(584, 315);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // ズームToolStripMenuItem
+            // 
+            this.ズームToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.拡大ToolStripMenuItem,
+            this.縮小ToolStripMenuItem,
+            this.標準ToolStripMenuItem});
+            this.ズームToolStripMenuItem.Name = "ズームToolStripMenuItem";
+            this.ズームToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ズームToolStripMenuItem.Text = "ズーム";
+            // 
+            // 拡大ToolStripMenuItem
+            // 
+            this.拡大ToolStripMenuItem.Name = "拡大ToolStripMenuItem";
+            this.拡大ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.拡大ToolStripMenuItem.Text = "拡大";
+            this.拡大ToolStripMenuItem.Click += new System.EventHandler(this.拡大ToolStripMenuItem_Click);
+            // 
+            // 縮小ToolStripMenuItem
+            // 
+            this.縮小ToolStripMenuItem.Name = "縮小ToolStripMenuItem";
+            this.縮小ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.縮小ToolStripMenuItem.Text = "縮小";
+            this.縮小ToolStripMenuItem.Click += new System.EventHandler(this.縮小ToolStripMenuItem_Click);
+            // 
+            // 標準ToolStripMenuItem
+            // 
+            this.標準ToolStripMenuItem.Name = "標準ToolStripMenuItem";
+            this.標準ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.標準ToolStripMenuItem.Text = "標準";
+            this.標準ToolStripMenuItem.Click += new System.EventHandler(this.標準ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "メモ帳";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 新しいウィンドウToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開くToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 名前を付けて保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ウィンドウを閉じるToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 終了ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 新規作成ToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 表示ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 太字ToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem ズームToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 拡大ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 縮小ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 標準ToolStripMenuItem;
+    }
+}
+
---
+++ memo/memo/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace memo
+{
+    public partial class Form1 : Form
+    {
+        // 保存済みかどうか(true：保存済み、false：未保存)
+        private bool isSave;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 新しいウィンドウToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 newForm = new Form1();
+            newForm.Show();
+        }
+
+        private void 開くToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "テキストファイル(*.txt)|*.txt";
+            ofd.Title = "開く";
+
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(ofd.FileName);
+            }
+        }
+
+        private void 保存ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog ofd = new SaveFileDialog();
+            ofd.Filter = "テキストファイル(*.txt)|*.txt";
+            ofd.Title = "保存";
+
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(ofd.FileName, richTextBox1.Text);
+            }
+        }
+
+        private void 名前を付けて保存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "テキスト ファイル (*.txt)|*.txt|すべてのファイル (*.*)|*.*";
+            saveFileDialog.Title = "名前を付けて保存";
+            saveFileDialog.DefaultExt = "txt";
+            saveFileDialog.FileName = "新しいファイル.txt"; // デフォルトのファイル名
+
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // テキストボックスの内容を指定したファイルに保存
+                try
+                {
+                    System.IO.File.WriteAllText(saveFileDialog.FileName, richTextBox1.Text);
+                    MessageBox.Show("ファイルが保存されました。", "保存完了", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("保存中にエラーが発生しました: " + ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void ウィンドウを閉じるToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void 終了ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("メモ帳を終了します", "終了", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if(result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void 新規作成ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.isSave)
+            {
+                richTextBox1.Clear();
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show("内容を保存しますか？", "メモ帳", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+                if(result == DialogResult.Yes)
+                {
+                    SaveFileDialog ofd = new SaveFileDialog();
+                    ofd.Filter = "テキストファイル(*.txt)|*.txt";
+                    ofd.Title = "保存";
+
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        File.WriteAllText(ofd.FileName, richTextBox1.Text);
+                    }
+                }
+                else if (result == DialogResult.No)
+                {
+                    this.richTextBox1.Text = string.Empty;
+                    this.isSave = true;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        private void 太字ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // 選択範囲のフォントを取得
+            Font currentFont = richTextBox1.SelectionFont;
+
+            if (currentFont != null)
+            {
+                // 現在のフォントが太字の場合は解除、それ以外は太字を適用
+                FontStyle newFontStyle = currentFont.Bold ? FontStyle.Regular : FontStyle.Bold;
+                richTextBox1.SelectionFont = new Font(currentFont, newFontStyle);
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            // ステータスバーに文字数を表示する
+            string text = string.Format("文字数 {0}", this.richTextBox1.Text.Length);
+            this.toolStripStatusLabel1.Text = text;
+
+            // テキストが更新されたら、未保存にする
+            this.isSave = false;
+        }
+
+        private void 拡大ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AdjustFontSize(2);
+            //RichTextBox richTextBox = this.Controls["richTextBox1"] as RichTextBox;
+            //if (richTextBox != null)
+            //{
+            //    float currentSize = richTextBox.Font.Size;
+            //    richTextBox.Font = new Font(richTextBox.Font.FontFamily, currentSize + 2);
+            //}
+        }
+
+        private void 縮小ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AdjustFontSize(-2);
+            //RichTextBox richTextBox = this.Controls["richTextBox1"] as RichTextBox;
+            //if (richTextBox != null)
+            //{
+            //    float currentSize = richTextBox.Font.Size;
+            //    if (currentSize > 6) // 最小サイズの制限
+            //    {
+            //        richTextBox.Font = new Font(richTextBox.Font.FontFamily, currentSize - 2);
+            //    }
+            //}
+        }
+
+        private void 標準ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetDefaultFontSize();
+            //RichTextBox richTextBox = this.Controls["richTextBox1"] as RichTextBox;
+            //if (richTextBox != null)
+            //{
+            //    richTextBox.Font = new Font(richTextBox.Font.FontFamily, 12); // 標準サイズにリセット
+            //}
+        }
+
+        // フォントサイズを調整する
+        private void AdjustFontSize(int sizeChange)
+        {
+            if (richTextBox1.SelectionLength > 0)
+            {
+                // 選択範囲の文字ごとに処理
+                int selectionStart = richTextBox1.SelectionStart;
+                int selectionLength = richTextBox1.SelectionLength;
+                for (int i = selectionStart; i < selectionStart + selectionLength; i++)
+                {
+                    richTextBox1.Select(i, 1);
+                    Font currentFont = richTextBox1.SelectionFont ?? richTextBox1.Font;
+                    FontStyle currentStyle = currentFont.Style;// 現在のフォントスタイル（太字、斜体など）を保持
+                    float newSize = Math.Max(6, currentFont.Size + sizeChange); // 最小サイズを6ptに制限
+                    richTextBox1.SelectionFont = new Font(currentFont.FontFamily, newSize, currentFont.Style);
+                }
+                richTextBox1.Select(selectionStart, selectionLength); // 選択範囲を復元
+            }
+            else
+            {
+                // 全体に適用
+                Font currentFont = richTextBox1.Font;
+                float newSize = Math.Max(6, currentFont.Size + sizeChange);
+                richTextBox1.Font = new Font(currentFont.FontFamily, newSize, currentFont.Style);
+            }
+        }
+
+        // フォントサイズを標準にリセットする
+        private void SetDefaultFontSize()
+        {
+            if (richTextBox1.SelectionLength > 0)
+            {
+                // 選択範囲の文字ごとに処理
+                int selectionStart = richTextBox1.SelectionStart;
+                int selectionLength = richTextBox1.SelectionLength;
+                for (int i = selectionStart; i < selectionStart + selectionLength; i++)
+                {
+                    richTextBox1.Select(i, 1);
+                    Font currentFont = richTextBox1.SelectionFont ?? richTextBox1.Font;
+                    richTextBox1.SelectionFont = new Font(currentFont.FontFamily, 12, currentFont.Style); // デフォルトサイズ12pt
+                }
+                richTextBox1.Select(selectionStart, selectionLength); // 選択範囲を復元
+            }
+            else
+            {
+                // 全体に適用
+                Font currentFont = richTextBox1.Font;
+                richTextBox1.Font = new Font(currentFont.FontFamily, 12, currentFont.Style);
+            }
+        }
+    }
+}
